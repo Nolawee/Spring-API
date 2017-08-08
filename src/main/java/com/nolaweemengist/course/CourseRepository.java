90577fc6034bae98e8a9bcb9ee15d88b9c2799f2
Nolawee Mengist <nome9478@colorado.edu>
2017-08-08 16:50:03 -0600

    Paging commit (broken @Params)

--- src/main/java/com/nolaweemengist/course/CourseRepository.java
+++ src/main/java/com/nolaweemengist/course/CourseRepository.java
@@ -1,6 +1,13 @@
 package com.nolaweemengist.course;
 
-import org.springframework.data.repository.CrudRepository;
 
-public interface CourseRepository extends CrudRepository<Course, Long>{
+import org.springframework.data.domain.Page;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.awt.print.Pageable;
+
+public interface CourseRepository extends PagingAndSortingRepository<Course, Long> {
+    Page<Course> findByTitleContaining(@Param("title") String title, Pageable page);
+
 }
